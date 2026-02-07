0889e0ec18fa21226402d3d1c09c57edd2e38877
Kitasake <kendouriku@gmail.com>
2026-02-07 20:34:01 +0900

    Update difficulty.js

--- logic/difficulty.js
+++ logic/difficulty.js
@@ -1,14 +1,14 @@
 export const DIFFICULTIES = {
   easy: {
     label: "Easy",
-    partyHP: 100
+    partyHP: 70
   },
   normal: {
     label: "Normal",
-    partyHP: 80
+    partyHP: 50
   },
   hard: {
     label: "Hard",
-    partyHP: 60
+    partyHP: 30
   }
 };
