866fa0ed687373cb282a963da79d6d256d673c33
Kitasake <kendouriku@gmail.com>
2026-02-07 19:41:41 +0900

    Create difficulty.js

---
+++ logic/difficulty.js
@@ -0,0 +1,14 @@
+export const DIFFICULTIES = {
+  easy: {
+    label: "Easy",
+    partyHP: 100
+  },
+  normal: {
+    label: "Normal",
+    partyHP: 80
+  },
+  hard: {
+    label: "Hard",
+    partyHP: 60
+  }
+};
